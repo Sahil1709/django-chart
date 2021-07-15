cfbd39d196a93e92948b3a4dfb51661c131aae28
Sahil <sahilofficial17@gmail.com>
2021-07-15 16:47:21 +0530

    2 charts

--- clubs/views.py
+++ clubs/views.py
@@ -9,8 +9,7 @@ class ClubChartView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["chartname"] = "Clubs Chart"
-        context["pagename"] = "Clubs"
+        context["pagename"] = "Clubs Chart"
         context["qs"] = Club.objects.all()
         return context
 
